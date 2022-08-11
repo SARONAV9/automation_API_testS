50cca29c7331e1cdb90cddc7f2d3a2dce0b949a5
SARONAV9 <www.snavasardyan23@gmail.com>
2022-08-11 23:08:41 +0400

    checking requests in one test, add delete request

--- Gorest_Requests/test_gorest.py
+++ Gorest_Requests/test_gorest.py
@@ -1,7 +1,6 @@
 
 from datetime import datetime
 import requests
-import pytest
 import yaml
 
 now = datetime.now()
@@ -14,15 +13,16 @@ status = "active"
 
 class Test(object):
     format = "json"
-    url = "https://gorest.co.in/public/v2/users"
+    url = "https://gorest.co.in/public/v2/users/"
 
     def setup_method(self):
         key_file = open("key.yaml", "r")
         setting = yaml.safe_load(key_file)
         self.access_token = setting["key"]
 
-    # Get users list, status code is 200
-    def test_get_users_list(self):
+    def test_requests(self):
+
+        # Get users list, status code is 200
         params = {"format": format, "access-token": self.access_token}
         response = requests.get(self.url, params=params)
         assert response.status_code == 200
@@ -32,22 +32,7 @@ class Test(object):
         response = requests.patch(self.url, params=params)
         assert response.status_code == 404
 
-    # Get a user, status code is 200
-    @pytest.mark.parametrize("user_id",[3425])
-    def test_get_a_user(self, user_id):
-        params = {"format":self.format,"access-token":self.access_token,"id":user_id}
-        response = requests.get(self.url, params=params)
-        assert response.status_code == 200
-
-        # Try to get a user, status code is 400
-        headers = {"Host": ""}
-        params = {"format":self.format,"access-token":self.access_token,"id":user_id}
-        response = requests.get(self.url, params=params, headers=headers)
-        assert response.status_code == 400
-
-    # Create a user, status code is 201
-    def test_create_a_user(self):
-        headers = {"Content-Type": "application/json; charset=utf-8"}
+        # Create a user, status code is 201
         data = {
             "name": name,
             "email": email,
@@ -56,18 +41,32 @@ class Test(object):
         }
         params = {"format": self.format, "access-token": self.access_token}
         response = requests.post(self.url, params=params, data=data)
-        print(response.json())
         assert response.status_code == 201
+        r = response.json()
+        user_id = (r["id"])
+
+        # Get a user, status code is 200
         params = {"format": self.format, "access-token": self.access_token, "name": name}
         response = requests.get(self.url, params=params)
         assert response.status_code == 200
+
+        # Try to get a user, status code is 400
+        headers = {"Host": ""}
+        params = {"format": self.format, "access-token": self.access_token, "name": name}
+        response = requests.get(self.url, params=params, headers=headers)
+        assert response.status_code == 400
 
         # Try to create an existing user, status code is 422
         params = {"format":self.format,"access-token":self.access_token,}
         response = requests.post(self.url, params=params, data=data)
         assert response.status_code == 422
 
-        # Create a user with wrong token, status code is 401
+        # Try to create a user with wrong token, status code is 401
         params = {"format":self.format,"access-token":"safa",}
         response = requests.post(self.url, params=params, data=data)
         assert response.status_code == 401
+
+    # Delete a user, status code is 204
+        params = {"format":self.format,"access-token":self.access_token}
+        response = requests.delete(self.url + str(user_id), params=params)
+        assert response.status_code == 204
